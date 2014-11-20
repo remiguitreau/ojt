d450b33f3ee39f6d29bef6c10f10b8076547621f
Rémi Guitreau <remi.guitreau@free.fr>
2014-11-20 21:41:03 +0100

    #1 Add timestamp identifier in weighing file name

--- ojt-core/src/main/java/com/ojt/tools/FileNameComposer.java
+++ ojt-core/src/main/java/com/ojt/tools/FileNameComposer.java
@@ -1,11 +1,11 @@
 package com.ojt.tools;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.ojt.CompetitionCategory;
 import com.ojt.CompetitionDescriptor;
 import com.ojt.OJTCompetitionCategories;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author CDa
@@ -13,79 +13,70 @@ import com.ojt.OJTCompetitionCategories;
  */
 public class FileNameComposer {
 
-	/**
-	 * Méthode de création du nom de fichier de réserve de compétiteurs pour un
-	 * prochain tri
-	 * 
-	 * @param competitionDescriptor
-	 * @return le nom du fichier
-	 */
-	public static String composeSavedFileForAnotherSortName(
-			final CompetitionDescriptor competitionDescriptor,
-			final String extension) {
-		return composeDirectoryName(competitionDescriptor) + "-reserve"
-				+ extension;
-	}
+    /**
+     * Méthode de création du nom de fichier de réserve de compétiteurs pour un
+     * prochain tri
+     * @param competitionDescriptor
+     * @return le nom du fichier
+     */
+    public static String composeSavedFileForAnotherSortName(
+            final CompetitionDescriptor competitionDescriptor, final String extension) {
+        return composeDirectoryName(competitionDescriptor) + "-reserve" + extension;
+    }
 
-	/**
-	 * Méthode de création du nom de fichier de poste
-	 * 
-	 * @param competitionDescriptor
-	 * @return le nom du fichier
-	 */
-	public static String composeWeighingPostFileName(
-			final CompetitionDescriptor competitionDescriptor,
-			final String weighingPost, final String extension) {
-		return composeDirectoryName(competitionDescriptor) + "-poste"
-				+ weighingPost + extension;
-	}
+    /**
+     * Méthode de création du nom de fichier de poste
+     * @param competitionDescriptor
+     * @return le nom du fichier
+     */
+    public static String composeWeighingPostFileName(final CompetitionDescriptor competitionDescriptor,
+            final String weighingPost, final String extension) {
+        return composeDirectoryName(competitionDescriptor) + "-poste" + weighingPost + "_"
+                + new SimpleDateFormat("dd.MM.yyyy.HH.mm.ss").format(new Date()) + extension;
+    }
 
-	/**
-	 * Méthode de création du nom de la sauvegarde
-	 * 
-	 * @param competitionDescriptor
-	 * @return le nom du fichier
-	 */
-	public static String composeFileName(
-			final CompetitionDescriptor competitionDescriptor,
-			final String extension) {
-		return composeDirectoryName(competitionDescriptor) + "-"
-				+ new SimpleDateFormat("dd.MM.yyyy.HH.mm").format(new Date())
-				+ extension;
-	}
+    /**
+     * Méthode de création du nom de la sauvegarde
+     * @param competitionDescriptor
+     * @return le nom du fichier
+     */
+    public static String composeFileName(final CompetitionDescriptor competitionDescriptor,
+            final String extension) {
+        return composeDirectoryName(competitionDescriptor) + "-"
+                + new SimpleDateFormat("dd.MM.yyyy.HH.mm").format(new Date()) + extension;
+    }
 
-	/**
-	 * Méthode de création du nom du répertoire de la manifestation
-	 * 
-	 * @param competitionDescriptor
-	 * @return le nom du répertoire de la manifestation
-	 */
-	public static String composeDirectoryName(
-			final CompetitionDescriptor competitionDescriptor) {
-		return (competitionDescriptor.getCategory() == CompetitionCategory.UNKNOWN_CATEGORY ? ""
-				: (OJTCompetitionCategories.formatHumanName(competitionDescriptor.getCategory().getHumanName()) + "-"))
-				+ competitionDescriptor.getCompetitionName().replaceAll(" ",
-						"_")
-				+ "-"
-				+ competitionDescriptor.getLocation().replaceAll(" ", "_")
-				+ "-" + competitionDescriptor.getDate().replaceAll(" ", "_");
-	}
+    /**
+     * Méthode de création du nom du répertoire de la manifestation
+     * @param competitionDescriptor
+     * @return le nom du répertoire de la manifestation
+     */
+    public static String composeDirectoryName(final CompetitionDescriptor competitionDescriptor) {
+        return (competitionDescriptor.getCategory() == CompetitionCategory.UNKNOWN_CATEGORY ? ""
+                : OJTCompetitionCategories.formatHumanName(competitionDescriptor.getCategory().getHumanName())
+                        + "-")
+                + competitionDescriptor.getCompetitionName().replaceAll(" ", "_")
+                + "-"
+                + competitionDescriptor.getLocation().replaceAll(" ", "_")
+                + "-"
+                + competitionDescriptor.getDate().replaceAll(" ", "_");
+    }
 
-	public static CompetitionDescriptor decomposeFileName(final String fileName) {
-		final CompetitionDescriptor competitionDescriptor = new CompetitionDescriptor();
-		final String file[] = fileName.split("-");
+    public static CompetitionDescriptor decomposeFileName(final String fileName) {
+        final CompetitionDescriptor competitionDescriptor = new CompetitionDescriptor();
+        final String file[] = fileName.split("-");
 
-		try {
-			competitionDescriptor.setCategory(OJTCompetitionCategories.retrieveCategoryFromHumanName(file[0]));
-			competitionDescriptor.setName(file[1]);
-			competitionDescriptor.setLocation(file[2]);
-			competitionDescriptor.setDate(file[3]);
-		} catch (final Exception ex) {
-			competitionDescriptor.setName(file[0]);
-			competitionDescriptor.setLocation(file[1]);
-			competitionDescriptor.setDate(file[2]);
-		}
+        try {
+            competitionDescriptor.setCategory(OJTCompetitionCategories.retrieveCategoryFromHumanName(file[0]));
+            competitionDescriptor.setName(file[1]);
+            competitionDescriptor.setLocation(file[2]);
+            competitionDescriptor.setDate(file[3]);
+        } catch (final Exception ex) {
+            competitionDescriptor.setName(file[0]);
+            competitionDescriptor.setLocation(file[1]);
+            competitionDescriptor.setDate(file[2]);
+        }
 
-		return competitionDescriptor;
-	}
+        return competitionDescriptor;
+    }
 }
